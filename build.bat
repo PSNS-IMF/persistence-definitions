e50ef123596a6276805fd1277f17dc175dd10ec9
Jeff Purcell <jeffrey.a.purcell@navy.mil>
2015-02-11 08:29:31 -0800

    Move test run into MSBuild project file.

--- build.bat
+++ build.bat
@@ -1,13 +1,3 @@
 @echo Off
 
 "%MsBuildExe%" MyGetBuild.proj /p:Configuration="%Configuration%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
-
-REM Unit Tests
-"%VsTestConsole%" test\Definitions.UnitTests\bin\%Configuration%\Definitions.UnitTests.dll
-if not "%errorlevel%"=="0" goto failure
-
-:success
-exit 0
-
-:failure
-exit -1
