27053a67c411950e92586517267e6295ad9e13da
Adam Purcell <ja9urc311@riseup.net>
2015-02-10 20:14:13 -0800

    Adding test run to build.

--- build.bat
+++ build.bat
@@ -16,8 +16,18 @@ if "%nuget%" == "" (
 
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Definitions.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
+REM Unit Tests
+%VsTestConsole% test\Definitions.UnitTests\bin\%config%\Definitions.UnitTests.dll
+if not "%errorlevel%"=="0" goto failure
+
 mkdir Build
 mkdir Build\lib
 mkdir Build\lib\net40
 
 %nuget% pack "src\Psns.Common.Persistence.Definitions\nuget\Psns.Common.Persistence.Definitions.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
+
+:success
+exit 0
+
+:failure
+exit -1
